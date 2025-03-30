4262752367c2abae01fc727be825fc17576f40d8
Teyfik Aktaş <aktasteyf@gmail.com>
2025-03-31 00:39:48 +0300

    seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,18 +2,26 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-        // User::factory(10)->create();
-
+        $this->call([
+            AnnouncementSeeder::class,
+            CategorySeeder::class,
+            CourseEnrollmentSeeder::class,
+            CourseSystemSeeder::class,
+            HomeworkSeeder::class,
+            ResourceSystemSeeder::class,
+            TestCoursesSeeder::class,
+            UpdateUserPhonesSeeder::class,
+        ]);
     }
 }
