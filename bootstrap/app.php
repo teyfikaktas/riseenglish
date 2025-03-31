8ba1338913b893cfc68e32f2365a745b8fec9910
Teyfik Aktaş <aktasteyf@gmail.com>
2025-03-31 03:07:41 +0300

    ensure

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
             'role' => \App\Http\Middleware\EnsureUserHasRole::class,
-            // varsa diğer alias'lar
+            'verified.phone' => \App\Http\Middleware\EnsurePhoneIsVerified::class, // Bu satırı ekleyin
         ]);
         })
     ->withExceptions(function (Exceptions $exceptions) {
