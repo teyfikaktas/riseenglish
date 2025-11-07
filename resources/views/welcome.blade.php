117491b9d115ede843d0ba9199a759f49eb645e0
Teyfik Aktaş <aktasteyf@gmail.com>
2025-11-07 18:37:34 +0300

    commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -965,32 +965,6 @@
                                 </div>
                             </div>
 
-                            <div class="video-slide flex-shrink-0 w-full md:w-1/2 lg:w-1/3 px-4">
-                                <div
-                                    class="bg-white rounded-xl shadow-lg overflow-hidden transition-all duration-300 hover:shadow-xl hover:-translate-y-2">
-                                    <div class="relative pb-[56.25%]"> <!-- 16:9 aspect ratio -->
-                                        <div class="video-thumbnail absolute inset-0" data-video-id="js-iBirRIJU">
-                                            <img src="https://i.ytimg.com/vi/js-iBirRIJU/hqdefault.jpg"
-                                                alt="Video thumbnail" class="w-full h-full object-cover">
-
-                                            <div
-                                                class="play-button absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-16 h-16 rounded-full bg-[#e63946] flex items-center justify-center z-10">
-                                                <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-white"
-                                                    fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                        d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z" />
-                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                        d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                                                </svg>
-                                            </div>
-                                        </div>
-                                        <div class="video-iframe-container absolute inset-0 hidden"></div>
-                                    </div>
-                                    <div class="p-4 text-center bg-[#1a2e5a] text-white">
-                                        <h3 class="font-semibold">Öğrencilerimiz</h3>
-                                    </div>
-                                </div>
-                            </div>
 
                             <div class="video-slide flex-shrink-0 w-full md:w-1/2 lg:w-1/3 px-4">
                                 <div
