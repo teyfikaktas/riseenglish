aaca4570b698cc82db0d26cf2aa24fb8997bf1fa
teyfikaktas <aktasteyf@gmail.com>
2026-05-19 01:13:53 +0300

    fix: update page title for improved branding

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
     <link rel="manifest" href="/site.webmanifest">
     
-    <title>Özel Ders Cepte | Online Özel Ders Platformu</title>
+    <title>Rise English</title>
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=inter:400,500,600,700" rel="stylesheet" />
